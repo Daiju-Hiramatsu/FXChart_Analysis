f3db3336f64df6115b02466064545fa08414963d
Daiju_Hiramatsu <trick@192.168.11.12>
2022-08-16 02:39:24 +0900

    read-data project create.

---
+++ read-data/src/main/java/application/DataReader.java
@@ -0,0 +1,16 @@
+package main.java.application;
+
+public class DataReader {
+
+	public static void main(String[] args) {
+		DataReader myserf = new DataReader();
+		//httpsからｈｔｍｌを読み取る。
+		//読み取ったら、そのドル円の情報を引っ張ってくる。
+		
+		//Mapなどを使って複数回のドル円情報を格納
+		//格納した情報から全体の近似直線と最近の近似直線を見る
+		//全体の近似直線（A）と最近の近似直線（B）のそれぞれMapなどに格納
+		
+	}
+
+}
